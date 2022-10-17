01845c2ce146009451b671ecbd7031c0b7c21ed6
Shawn W <78707396+sb-son@users.noreply.github.com>
2022-10-17 11:41:05 -0500

    starting iterating arrays exercise. todo 1 complete

---
+++ js/iterating.js
@@ -0,0 +1,48 @@
+(function(){
+    "use strict";
+
+    /**
+     * TODO 1:
+     * Create an array of 4 people's names and store it in a variable called
+     * 'names'.
+     */
+    let names = ['shawn', 'antonio', 'isaac', 'javier']
+
+    /**
+     * TODO 2:
+     * Create a log statement that will log the number of elements in the names
+     * array.
+     */
+
+    /**
+     * TODO 3:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+     */
+
+    /**
+     * TODO 4:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+
+    /**
+     * TODO 5:
+     * Refactor your above code to use a `forEach` loop
+     */
+
+    /**
+     * TODO 6:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+
+})();
