f9362b249f8e5c75f1e600cca6ddf4045f8a2500
Shawn W <78707396+sb-son@users.noreply.github.com>
2022-10-17 17:13:08 -0500

    added new answer for removing 'The Sun'

--- js/planets-array.js
+++ js/planets-array.js
@@ -27,7 +27,8 @@
     console.log(planets);
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
-    planets.shift()
+    planets.splice(0,1)
+    //planets.shift()
     console.log(planets);
 
     console.log('Removing "Pluto" from the end of the planets array.');
