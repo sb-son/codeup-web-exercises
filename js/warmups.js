e559c06c6885010d43ca9b84ba90d026e627fe26
Shawn W <78707396+sb-son@users.noreply.github.com>
2022-10-31 09:39:39 -0500

    minor adjustments to fizzBuzz function

--- js/warmups.js
+++ js/warmups.js
@@ -130,13 +130,15 @@ console.log(getTallestHamster(hamsters));;
 // All other numbers should be printed in the console.
 
 function fizzBuzz() {
-    for (let i = 0; i < 100; i++) {
+    for (let i = 1; i <= 100; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
-            console.log(i + " FizzBuzz")
+            console.log("FizzBuzz")
         } else if (i % 3 === 0) {
-            console.log(i + " Fizz")
+            console.log("Fizz")
         } else if (i % 5 === 0) {
-            console.log(i + " Buzz")
+            console.log("Buzz")
+        } else {
+            console.log(i)
         }
     }
 }
