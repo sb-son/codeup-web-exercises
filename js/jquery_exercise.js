2a941eb4a2415ebd9041515491d1ee03e327ef0b
Shawn W <78707396+sb-son@users.noreply.github.com>
2022-11-17 16:10:08 -0600

    completed mouse events exercise

--- js/jquery_exercise.js
+++ js/jquery_exercise.js
@@ -7,9 +7,24 @@ $(function() {
     // let codeup = $('.codeup');
     // codeup.css("border", "1px solid red")
     let listItems = $('li');
-    listItems.css("font-size", '20px');
-    let mainContents = $('h1, p, li');
-    mainContents.css("background-color", "lightblue")
-    alert($('h1').html())
+    // listItems.css("font-size", '20px');
+    // let mainContents = $('h1, p, li');
+    // mainContents.css("background-color", "lightblue")
+    // alert($('h1').html())
 
+    $('h1').click(function () {
+    $('h1').css('background-color', 'firebrick')
+    });
+
+    $('p').dblclick(function () {
+        $(this).css('font-size', '18px')
+    });
+
+    listItems.hover(
+function () {
+            listItems.css('color', 'red')
+        },
+function () {
+            listItems.css('color', 'black')
+        });
 });
