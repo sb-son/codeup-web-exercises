3dbbe66de3faeca6706f81aa33d14da78aa947a1
Shawn W <78707396+sb-son@users.noreply.github.com>
2022-12-08 09:20:53 -0600

    building github function

--- js/promises.js
+++ js/promises.js
@@ -1,9 +1,15 @@
 "use strict";
 (() => {
-    fetch(`https://api.github.com/users/sb-son/events/public`, {headers: {'Authorization': GITHUB_KEY}}).then(resp => resp.json()).then(data => {
-        const pullRequests = data.map(requests => data.type)
-        console.log(pullRequests)
-        console.log(data)
-    })
+    function lastPushDate(username) {
+        fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': GITHUB_KEY}}).then(resp => resp.json()).then(data => {
+            const pullRequests = data.map(requests => requests.type)
+            let firstRequest = pullRequests.indexOf("PullRequestEvent")
+            console.log(firstRequest)
+            console.log(pullRequests)
+            console.log(data[firstRequest].created_at)
+            return data[firstRequest].created_at
+        })
+    }
+    lastPushDate('sb-son')
 
 })();
